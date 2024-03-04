7a340b016768f07ed57918dfbe91c52d241e5ef9
leemfil <99686683+leemfil@users.noreply.github.com>
2024-03-05 00:50:32 +0100

    Update woordenlijst.py

--- Python/Lists/Woorden in lijst/solutions/woordenlijst.py
+++ Python/Lists/Woorden in lijst/solutions/woordenlijst.py
@@ -1,1 +1,24 @@
+aantal = int(input("Geef het aantal woorden: "))
 
+woorden_lijst = []
+for woord in range(aantal):
+    woord = input("Geef het volgende woord: ")
+    woorden_lijst.append(woord)
+print(woorden_lijst)
+
+woorden_lijst.sort()
+print(woorden_lijst)
+
+print(woorden_lijst[0])
+
+lijst_meer_dan_5 = []
+lijst_minder_dan_5 = []
+for woord in woorden_lijst:
+    if len(woord) > 5:
+        lijst_meer_dan_5.append(woord)
+    else:
+        lijst_minder_dan_5.append(woord)
+print(lijst_meer_dan_5)
+print(lijst_minder_dan_5)
+
+print(lijst_meer_dan_5 + lijst_minder_dan_5)
