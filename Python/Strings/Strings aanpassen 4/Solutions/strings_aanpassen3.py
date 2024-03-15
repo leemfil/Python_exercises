81e659812dc358c90c0ef1b8a5b2e15ab20247f8
leemfil <99686683+leemfil@users.noreply.github.com>
2024-03-15 10:48:56 +0100

    Update strings_aanpassen3.py

--- Python/Strings/Strings aanpassen 4/Solutions/strings_aanpassen3.py
+++ Python/Strings/Strings aanpassen 4/Solutions/strings_aanpassen3.py
@@ -2,25 +2,23 @@ tekst1 = input("Geef tekst 1: ")
 tekst2 = input("Geef tekst 2: ")
 tekst3 = input("Geef tekst 3: ")
 
-samengevoegd = tekst2 + " " + tekst3
-print(samengevoegd)
+pos_i = tekst2.find("i")
+print(tekst2[pos_i + 1:])
 
-pos_punt = tekst1.find(".")
-substring1 = tekst1[:pos_punt]
-substring2 = tekst1[pos_punt + 1:]
-print(substring1.capitalize())
-print(substring2.capitalize())
+substring1 = tekst1.replace(".be", "").capitalize()
+print(substring1)
 
-pos_spatie = tekst2.find(" ")
-eerste_woord = tekst2[:pos_spatie]
-print(f"{eerste_woord} heeft een lengte van {len(eerste_woord)}.")
+voornaam, domein = tekst1.split(".")
+print(voornaam)
+print(domein)
 
-zonder_spaties = tekst3.replace(" ", "")
-print(zonder_spaties)
+lengte = len(tekst3)
+print(tekst3 + f": lengte {lengte}")
 
 teller = 0
 for letter in tekst2:
-    if letter in "aeiou":
+    if letter in " .,;:?":
         teller += 1
 
-print(f"Het aantal klinkers in tekst 2 is {teller}.")
+print(f"Het aantal spaties en tekens in tekst 2 is {teller}.")
+
