0d537c5e2f94e6535b6c8ad46de421d1bba3bd2d
leemfil <99686683+leemfil@users.noreply.github.com>
2024-06-08 14:44:49 +0200

    Update woordenlijst.py

--- Python/Functies/Woorden aanpassen/solutions/woordenlijst.py
+++ Python/Functies/Woorden aanpassen/solutions/woordenlijst.py
@@ -1,9 +1,9 @@
-def selecteer(woordenlijst):
-    woordenlijst.sort()
-    
+def aanpassen(woordenlijst):
+    woordenlijst.reverse()
+
     nieuwe_lijst = []
     for woord in woordenlijst:
-        if len(woord) > 5:
+        if len(woord) <= 6:
             nieuwe_lijst.append(woord)
     
     return nieuwe_lijst
