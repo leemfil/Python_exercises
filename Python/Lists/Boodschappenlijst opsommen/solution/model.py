b16786c663bf22fe9002e15956c5bb92048c4f5b
leemfil <99686683+leemfil@users.noreply.github.com>
2026-02-22 14:02:39 +0100

    Add loop to print shopping list items

--- Python/Lists/Boodschappenlijst opsommen/solution/model.py
+++ Python/Lists/Boodschappenlijst opsommen/solution/model.py
@@ -1,1 +1,6 @@
+# For-lus 01 — Boodschappenlijst afdrukken
 
+boodschappen = ["eieren", "kaas", "hesp", "boter"]
+
+for boodschap in boodschappen:
+    print(f"Een boodschap in de lijst is : {boodschap}.")
