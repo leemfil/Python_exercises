d531d0047e8d71932ca4d2de30985f9f9173a3d8
leemfil <99686683+leemfil@users.noreply.github.com>
2026-03-03 07:33:50 +0100

    1st commit

---
+++ Python/Lists/Kleurenlijst aanpassen/solution/model.py
@@ -0,0 +1,9 @@
+# Slicing 01 — Eerste en laatste fruit
+
+fruitmand = ["appel", "banaan", "kiwi", "peer", "mango", "druif"]
+
+eerste_drie = fruitmand[:3]
+laatste_twee = fruitmand[-2:]
+
+print(eerste_drie)
+print(laatste_twee)
