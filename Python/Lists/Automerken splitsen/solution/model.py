a73211249aa09e0cb10ee34e00623b0771f6298e
leemfil <99686683+leemfil@users.noreply.github.com>
2026-03-07 03:31:09 +0100

    Refactor code to split car brands by name length

--- Python/Lists/Automerken splitsen/solution/model.py
+++ Python/Lists/Automerken splitsen/solution/model.py
@@ -1,8 +1,15 @@
-# Spel — Strafpunten berekenen
+# Automerken opsplitsen op basis van lengte
 
-spelers = ["Emma", "Lucas", "Noor", "Liam", "Mila"]
+merken = ["Audi", "Volvo", "Toyota", "Kia", "Renault", "Subaru"]
 
-for i in range(len(spelers)):
-    naam = spelers[i]
-    strafpunten = (i + 1) * 2
-    print(f"{naam} heeft {strafpunten} strafpunten verzameld.")
+lijst1 = []
+lijst2 = []
+
+for merk in merken:
+    if len(merk) > 5:
+        lijst2.append(merk)
+    else:
+        lijst1.append(merk)
+
+print(f"De merken met 5 letters of minder zijn {lijst1}.")
+print(f"De merken met meer dan 5 letters zijn {lijst2}.")
