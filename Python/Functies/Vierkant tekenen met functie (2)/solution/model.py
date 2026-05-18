25db3d6c9ef3c79216f7af68a37558446fa1d8d5
leemfil <99686683+leemfil@users.noreply.github.com>
2026-05-18 23:06:28 +0200

    Refactor square drawing function and input handling
    Moved turtle initialization inside the function and changed the input method for side length.

--- Python/Functies/Vierkant tekenen met functie (2)/solution/model.py
+++ Python/Functies/Vierkant tekenen met functie (2)/solution/model.py
@@ -1,14 +1,15 @@
 import turtle
 
-pen = turtle.Turtle()
-
 
 def teken_vierkant(lengte):
+    pen = turtle.Turtle()
+
     for _ in range(4):
         pen.forward(lengte)
         pen.left(90)
 
 
-teken_vierkant(120)
+zijde = int(input("Geef de lengte van de zijde: "))
+teken_vierkant(zijde)
 
 turtle.exitonclick()
