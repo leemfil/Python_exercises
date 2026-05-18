47dc3c0dbccd78f88df72bffcf9b3d4d5f736871
leemfil <99686683+leemfil@users.noreply.github.com>
2026-05-19 00:34:52 +0200

    Implement sprite and polygon drawing functions
    Added functions to draw sprites and polygons using turtle graphics.

--- Python/Functies/Veelhoek met sprites/solution/model.py
+++ Python/Functies/Veelhoek met sprites/solution/model.py
@@ -1,9 +1,31 @@
 import turtle
 
 pen = turtle.Turtle()
+pen.speed(0)
 
-for _ in range(4):
-    pen.forward(200)
-    pen.left(90)
+
+def teken_sprite(lengte):
+    draaihoek = 360 / 8
+
+    for _ in range(8):
+        pen.forward(lengte)
+        pen.backward(lengte)
+        pen.left(draaihoek)
+
+
+def teken_veelhoek(lengte, aantal_zijden):
+    draaihoek = 360 / aantal_zijden
+    lengte_sprite = lengte / 2
+
+    for _ in range(aantal_zijden):
+        teken_sprite(lengte_sprite)
+        pen.forward(lengte)
+        pen.left(draaihoek)
+
+
+zijde = int(input("Geef de lengte van een zijde: "))
+aantal = int(input("Geef het aantal zijden: "))
+
+teken_veelhoek(zijde, aantal)
 
 turtle.exitonclick()
