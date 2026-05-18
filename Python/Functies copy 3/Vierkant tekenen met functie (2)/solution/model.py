7d92895d2834ae49432b3e0c25af94804a735599
leemfil <99686683+leemfil@users.noreply.github.com>
2026-05-18 23:28:09 +0200

    1st commit

--- Python/Functies copy 3/Vierkant tekenen met functie (2)/solution/model.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-pen = turtle.Turtle()
-
-for _ in range(4):
-    pen.forward(200)
-    pen.left(90)
-
-turtle.exitonclick()
--- Python/Functies copy 3/Woorden aanpassen/solutions/woordenlijst.py
+++
@@ -1,9 +0,0 @@
-def aanpassen(woordenlijst):
-    woordenlijst.reverse()
-
-    nieuwe_lijst = []
-    for woord in woordenlijst:
-        if len(woord) <= 6:
-            nieuwe_lijst.append(woord)
-    
-    return nieuwe_lijst
