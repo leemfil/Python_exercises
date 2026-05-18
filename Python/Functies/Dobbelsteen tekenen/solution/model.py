545ba831e117c60c987a3a34f41da436d4cb74e7
leemfil <99686683+leemfil@users.noreply.github.com>
2026-05-19 00:07:12 +0200

    Update model.py

--- Python/Functies/Dobbelsteen tekenen/solution/model.py
+++ Python/Functies/Dobbelsteen tekenen/solution/model.py
@@ -2,8 +2,30 @@ import turtle
 
 pen = turtle.Turtle()
 
-for _ in range(4):
-    pen.forward(200)
-    pen.left(90)
+def ga_naar(x, y):
+    pen.penup()
+    pen.goto(x, y)
+    pen.pendown()
+
+
+def teken_stip(x, y):
+    ga_naar(x, y)
+    pen.dot(20)
+
+
+def teken_vierkant():
+    ga_naar(-50, -50)
+
+    for _ in range(4):
+        pen.forward(100)
+        pen.left(90)
+
+
+teken_vierkant()
+teken_stip(-25, 25)
+teken_stip(25, 25)
+teken_stip(0, 0)
+teken_stip(-25, -25)
+teken_stip(25, -25)
 
 turtle.exitonclick()
