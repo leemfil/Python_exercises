c97ee23d9e7e952759916c87d8dc732d6caab7dc
leemfil <99686683+leemfil@users.noreply.github.com>
2026-09-14 07:23:31 +0200

    Refactor model.py to evaluate number sign
    Removed turtle graphics code and added logic to check if a number is positive, negative, or zero.

--- Python/Condities/Positief, negatief of nul/solution/model.py
+++ Python/Condities/Positief, negatief of nul/solution/model.py
@@ -1,13 +1,8 @@
-import turtle
+getal = int(input())
 
-
-def teken_vierkant():
-    pen = turtle.Turtle()
-
-    for _ in range(4):
-        pen.forward(100)
-        pen.left(90)
-
-
-teken_vierkant()
-turtle.exitonclick()
+if getal > 0:
+    print("positief")
+elif getal < 0:
+    print("negatief")
+else:
+    print("nul")
