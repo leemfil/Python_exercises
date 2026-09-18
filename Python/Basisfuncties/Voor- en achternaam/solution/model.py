c3dd3d9450a6c4d5058b7da5602f5d17a6280be7
leemfil <99686683+leemfil@users.noreply.github.com>
2026-09-18 08:01:33 +0200

    Update model.py

--- Python/Basisfuncties/Voor- en achternaam/solution/model.py
+++ Python/Basisfuncties/Voor- en achternaam/solution/model.py
@@ -1,9 +1,4 @@
-import turtle
+voornaam = input()
+achternaam = input()
 
-pen = turtle.Turtle()
-
-for _ in range(4):
-    pen.forward(200)
-    pen.left(90)
-
-turtle.exitonclick()
+print(voornaam, achternaam)
