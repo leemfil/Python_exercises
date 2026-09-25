b6333164bf941e46447b8a9dfd7dde2e6d1239fa
leemfil <99686683+leemfil@users.noreply.github.com>
2026-09-25 09:00:14 +0200

    Implement Racewagen class with methods
    Added a Racewagen class with methods for acceleration and comparison.

--- Python/OOP/Classes/Racewagen/solution/solution.py
+++ Python/OOP/Classes/Racewagen/solution/solution.py
@@ -1,5 +1,30 @@
-zijde = float(input("Geef de lengte van een zijde: "))
 
-volume = zijde ** 3
+## `solution.py`
 
-print("Volume:", volume)
+class Racewagen:
+    def __init__(self, naam, snelheid, rondetijd):
+        self.naam = naam
+        self.snelheid = snelheid
+        self.rondetijd = rondetijd
+
+    def versnel(self, extra):
+        self.snelheid += extra
+
+    def __str__(self):
+        return f"{self.naam} | snelheid: {self.snelheid} km/u | rondetijd: {self.rondetijd} s"
+
+    def __eq__(self, other):
+        return self.snelheid == other.snelheid
+
+    def __mul__(self, other):
+        if self.rondetijd < other.rondetijd:
+            return f"{self.naam} wint!"
+        elif self.rondetijd > other.rondetijd:
+            return f"{other.naam} wint!"
+        else:
+            return "Gelijkspel!"
+
+
+wagen1 = Racewagen("Falcon", 280, 74)
+wagen2 = Racewagen("Comet", 280, 70)
+wagen3 = Racewagen("Viper", 310, 71)
